574af9de96b0dafd335996f10c9d1044a51f03a6
naufal <ibnunaufal5758@gmail.com>
2023-11-06 17:19:23 +0700

    fix recording

--- app/src/main/java/com/naufall/voicerecord/MainActivity.kt
+++ app/src/main/java/com/naufall/voicerecord/MainActivity.kt
@@ -2,18 +2,23 @@ package com.naufall.voicerecord
 
 import android.annotation.SuppressLint
 import android.annotation.TargetApi
+import android.app.DownloadManager
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.io.IOException
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     private var output: String? = null
@@ -30,14 +35,13 @@ class MainActivity : AppCompatActivity() {
         button_start_recording = findViewById(R.id.button_start_recording)
         button_stop_recording = findViewById(R.id.button_stop_recording)
         button_pause_recording = findViewById(R.id.button_pause_recording)
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(this, permissions,0)
+        }
 
-        mediaRecorder = MediaRecorder()
-        output = Environment.getExternalStorageDirectory().absolutePath + "/recording.mp3"
-
-        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
-        mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-        mediaRecorder?.setOutputFile(output)
+//        output = Environment.getExternalStorageDirectory().absolutePath + "/${timeNow}recording.mp3"
 
         button_start_recording?.setOnClickListener {
             if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
@@ -59,7 +63,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startRecording() {
+        Log.d("start", "startRecording")
+        Toast.makeText(this, "startRecording!", Toast.LENGTH_SHORT).show()
         try {
+            val timeNow = System.currentTimeMillis()
+            output = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/${timeNow}-recording.mp3"
+            mediaRecorder?.setOutputFile(output)
             mediaRecorder?.prepare()
             mediaRecorder?.start()
             state = true
@@ -71,6 +80,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        val timeNow = System.currentTimeMillis()
+        output = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/${timeNow}-recording.mp3"
+
+        mediaRecorder = MediaRecorder()
+        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+        mediaRecorder?.setOutputFile(output)
+    }
     @SuppressLint("RestrictedApi", "SetTextI18n")
     @TargetApi(Build.VERSION_CODES.N)
     private fun pauseRecording() {
@@ -100,8 +120,30 @@ class MainActivity : AppCompatActivity() {
             mediaRecorder?.stop()
             mediaRecorder?.release()
             state = false
+            openFile()
         }else{
             Toast.makeText(this, "You are not recording right now!", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaRecorder?.reset()
+    }
+    private fun openFile(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Berhasil")
+        builder.setMessage("Buka file?")
+
+        builder.setPositiveButton("Buka") { dialog, which ->
+            val i = Intent(DownloadManager.ACTION_VIEW_DOWNLOADS)
+            startActivity(i);
+        }
+
+        builder.setNegativeButton("Tutup") { dialog, which ->
+
+        }
+
+        builder.show()
     }
 }
